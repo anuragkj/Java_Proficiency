eb4717271067b2652ab03122fbf6150611cbf517
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2021-09-01 18:32:20 +0400

    Java

---
+++ Solution_2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution_2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        int a1 = Integer.parseInt(a);
+        
+        String b = scan.next();
+        int b1 = Integer.parseInt(b);
+        
+        String c = scan.next();
+        int c1 = Integer.parseInt(c);
+        
+        System.out.print(a1 + "\n" + b1 + "\n" +c1);
+        
+    }
+}
