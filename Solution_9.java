794726f7d17a025893a06e9ee30d5464a15e8118
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2021-09-03 18:00:08 +0400

    Java

---
+++ Solution_9.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution_9 {
+
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int i = 0;
+    while(scan.hasNext()){
+        i++;
+        System.out.println(i + " " + scan.nextLine());
+    }
+}
+}
