cdc697a811706972eb94abb74169ca0532757399
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2021-09-03 18:37:30 +0400

    Java

---
+++ Solution_10.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution_10 {
+static Scanner scan = new Scanner(System.in);
+static int B = scan.nextInt();
+static int H = scan.nextInt();
+static boolean flag = true;
+static{
+    try{
+        if(B <= 0 || H <= 0){
+            flag = false;
+            throw new Exception("Breadth and height must be positive");
+        }
+    }catch(Exception e){
+        System.out.println(e);
+    }
+
+}
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
