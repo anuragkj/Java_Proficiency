8343f7e158b9352789e8bbe734267590fba73c00
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2021-09-05 17:33:32 +0400

    Javaxit

---
+++ Solution_19.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Solution_19 {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0,k);
+        String largest = s.substring(0,k);
+        
+        for(int i =0; i+k <= s.length(); i++)
+        {
+            String sub = s.substring(i,i+k);
+            if(sub.compareTo(largest) >= 0)
+            {
+                largest = sub;
+                continue;
+            }
+            else if(sub.compareTo(smallest) <= 0)
+            {
+                smallest = sub;
+                continue;
+            }
+        }
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        
+        return smallest + "\n" + largest;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
