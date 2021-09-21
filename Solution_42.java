f91051aea8b250a7c08aaca58f9b3b9835787717
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2021-09-21 17:13:35 +0400

    Java

--- Solution_42.java
+++ Solution_42.java
@@ -1,6 +1,15 @@
 import java.util.*;
 
-// Write your Checker class here
+class Checker implements Comparator<Player>{
+    @Override
+    public int compare(Player a, Player b){
+        if (a.score == b.score){
+            return a.name.compareTo(b.name);//alphabetically
+        } else {
+            return b.score - a.score;//decreasing 
+        }
+    }
+}
 
 class Player{
     String name;
